4336f0c8e15984ce6138b2f8b7f7ee00bf6a9be7
Rene Hanslin <rene.hanslin@hseag.com>
2021-12-06 20:13:07 +0100

    QR code scanner worker (callback to .NET is not yet working)

--- BlazorWebcam/wwwroot/webcam.js
+++ BlazorWebcam/wwwroot/webcam.js
@@ -1,5 +1,9 @@
-function startVideo(src) {
+let dhHelper;
+
+function startVideo(dotNetHelper, videoElementName, canvasVideoName, canvasTrackName) {
     console.log("webcam.js: startVideo");
+    console.log(dotNetHelper);
+    dhHelper = dotNetHelper;
 
     if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
         navigator.mediaDevices.enumerateDevices().then(devices => {
@@ -29,7 +33,13 @@ function startVideo(src) {
         }
 
         navigator.mediaDevices.getUserMedia(constraintsVideo).then((stream) => {
-            let video = document.getElementById(src);
+            let video = document.getElementById(videoElementName);
+            let canvasVideo = document.getElementById(canvasVideoName);
+            let canvasTrack = document.getElementById(canvasTrackName);
+            let lastCode = null;
+            let lastScan = 0;
+            let scanInterval = 250;
+
             if ("srcObject" in video) {
                 video.srcObject = stream;
             } else {
@@ -40,6 +50,9 @@ function startVideo(src) {
                 console.log(e);
                 video.play();
             };
+
+            requestAnimationFrame((ts) => onAnimationFrame(dotNetHelper, ts, video, canvasVideo, canvasTrack, lastScan, scanInterval, lastCode));
+
             //mirror image
             //video.style.webkitTransform = "scaleX(-1)";
             //video.style.transform = "scaleX(-1)";
@@ -49,11 +62,120 @@ function startVideo(src) {
     console.log("webcam.js: startVideo - completed");
 }
 
-function getFrame(src, dest, dotNetHelper) {
+function getFrame(dotNetHelper, src, dest) {
     let video = document.getElementById(src);
     let canvas = document.getElementById(dest);
-    canvas.getContext('2d').drawImage(video, 0, 0, 320, 240);
+    let ctx = canvas.getContext('2d');
+
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: "dontInvert",
+    });
+
+    console.log(`code: ${code}`);
+    console.log(code);
 
     let dataUrl = canvas.toDataURL("image/jpeg");
     dotNetHelper.invokeMethodAsync('ProcessImage', dataUrl);
+}
+
+function onAnimationFrame(dotNetHelper, timestamp, video, canvasVideo, canvasTrack, lastScan, scanInterval, lastCode) {
+    if (timestamp - lastScan >= scanInterval) {
+        lastScan = timestamp;
+
+        let ctx = canvasVideo.getContext('2d');
+        ctx.drawImage(video, 0, 0, canvasVideo.width, canvasVideo.height);
+
+        var imageData = ctx.getImageData(0, 0, canvasVideo.width, canvasVideo.height);
+        var code = jsQR(imageData.data, imageData.width, imageData.height, {
+            inversionAttempts: "dontInvert",
+        });
+
+        if (code !== null) {
+            if (canvasTrack != null) {
+                canvasTrack.width = canvasVideo.width;
+                canvasTrack.height = canvasVideo.height;
+
+                // greenyellow = #FFADFF2F
+                // red = #FF3B58
+                const colorFill = "#90EE9050"; // #90EE90 = lightgreen
+                const colorStroke = "#90EE90AA"; // #90EE90 = lightgreen
+                const lineWidthStroke = 2;
+                drawTrackFrame(
+                    canvasTrack,
+                    code.location.topLeftCorner,
+                    code.location.topRightCorner,
+                    code.location.bottomRightCorner,
+                    code.location.bottomLeftCorner,
+                    colorFill,
+                    colorStroke,
+                    lineWidthStroke
+                );
+            }
+
+            let emitEvent = true;
+            if (lastCode !== null) {
+                if (lastCode.data === code.data) {
+                    emitEvent = false;
+                }
+            }
+
+            if (emitEvent) {
+                console.log(`code: ${code.data}`);
+                console.log(dotNetHelper);
+                dhHelper.invokeMethodAsync('QRCodeDetected', `XX: ${timestamp}`);
+            }
+
+            lastCode = code;
+        } else {
+            if (lastCode != null) {
+                if (canvasTrack != null) {
+                    canvasTrack.width = canvasVideo.width;
+                    canvasTrack.height = canvasVideo.height;
+
+                    const colorFill = "#00000000";
+                    const colorStroke = "#00000000";
+                    const lineWidthStroke = 0;
+                    drawTrackFrame(
+                        canvasTrack,
+                        lastCode.location.topLeftCorner,
+                        lastCode.location.topRightCorner,
+                        lastCode.location.bottomRightCorner,
+                        lastCode.location.bottomLeftCorner,
+                        colorFill,
+                        colorStroke,
+                        lineWidthStroke
+                    );
+                }
+
+                console.log(`code: **NO CODE **`);
+                dhHelper.invokeMethodAsync('QRCodeDetected', '');
+                lastCode = null;
+            }
+        }
+    }
+
+    requestAnimationFrame((ts) => onAnimationFrame(dotNetHelper, ts, video, canvasVideo, canvasTrack, lastScan, scanInterval, lastCode));
+}
+
+function drawTrackFrame(canvas, topLeft, topRight, bottomRight, bottomLeft, colorFill, colorStroke, lineWidthStroke) {
+    if(canvas != null) {
+        const ctx = canvas.getContext("2d");
+
+        if (ctx != null) {
+            ctx.beginPath();
+            ctx.moveTo(topLeft.x, topLeft.y);
+            ctx.lineTo(topRight.x, topRight.y);
+            ctx.lineTo(bottomRight.x, bottomRight.y);
+            ctx.lineTo(bottomLeft.x, bottomLeft.y);
+            ctx.closePath();
+            ctx.fillStyle = colorFill;
+            ctx.fill();
+            ctx.lineWidth = lineWidthStroke;
+            ctx.strokeStyle = colorStroke;
+            ctx.stroke();
+        }
+    }
 }
